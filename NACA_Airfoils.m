d8fbfc8e0de86df41b275220ddba52fb7f4de43d
marrazolav <maar8634@colorado.edu>
2026-03-29 17:08:40 -0600

    Refactor NACA_Airfoils function implementation
    Refactor NACA_Airfoils function to improve structure and clarity.

--- NACA_Airfoils.m
+++ NACA_Airfoils.m
@@ -1,34 +1,42 @@
-function [x_b,y_b,x_cam, y_cam] = NACA_Airfoils(m,p,t,c,N)
+
+%cosine spacing 
+theta=linspace(-pi,pi,N+1);
+x= (c/2)*(1-cos(theta));
 
 %Thicknesss distribution 
-yt= (c*t/0.2)*((0.2969*sqrt(x/c)) - (0.126*(x/c))-(0.3516*(x/c)^2)+(0.2843*(x/c)^3)-(0.1036*(x/c)^4));
+yt= (c*t/0.2)*((0.2969*sqrt(x/c))- (0.126*(x/c))-(0.3516*(x/c).^2)+(0.2843*(x/c).^3)-(0.1036*(x/c).^4));
 
-% camber lineand slope 
+% camber line and slope as vectors
 if p==0
 yc= zeros(size(x));
-dyc_dx= zeros(size(x));
-end
-
-% For NACA 2421
-%if(x>=0) && (x<pc)
-   % yc= m*(x/p^2)*(2*p-(x/c));
-%elseif (x>= pc) && (c<=x)
-% yc= m*(((c-x)/(1-p)^2)*(1+(x/c)-2*p));
-%else
-%    fprintf('Error');
-%end
-
-if 
+dycdx= zeros(size(x));
+else
+    x_cam= x/c;
+    yc=(m/p^2)*(2*p*x_cam-x_cam.^2) .*(x_cam <=p)+ (m/(1-p)^2)*((1-2*p)+ 2*p*x_cam- x_cam.^2) .*(x_cam>p);
+    dycdx= (2*m/p^2)*(p-x_cam).*(x_cam <= p)+(2*m/(1-p)^2)*(p-x_cam).*(x_cam >p);
 end
 
 % Angle
-Xi= arctan(dyc_dx);
+Xi= atan(dycdx);
 
 % Upper Surface
-xU= x-yt*sin(Xi);
-yU= yc+yt*cos(Xi);
+xU= x-yt .*sin(Xi);
+yU= yc+yt .*cos(Xi);
 
 %Lower Surface
-xL= x+yt*sin(Xi);
-yL= yc+yt*cos(Xi);
+xL= x+yt .*sin(Xi);
+yL= yc-yt.*cos(Xi);
+
+%% Combine surfaces into one (clockwise)
+% Upper surface Ttailing edge to leading edge
+xU= flip(xU);
+yU= flip(yU);
+
+% Lower surface leading edge to trailing edge
+xL= xL(2:end);   %skip 1st point of the lower surface so doesn't duplicate Leaing edge
+yL= yL(2:end);
+
+% Final boundary points
+x_b= [xU,xL];
+y_b= [yU,yL];
 end
