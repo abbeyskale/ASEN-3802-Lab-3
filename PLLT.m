54e33ac7fc5c22ae1c0b0208bc2b77a3a7ae28dc
abbeyskale <absk3746@colorado.edu>
2026-04-14 17:34:23 -0600

    working PLLT

--- PLLT.m
+++ PLLT.m
@@ -19,51 +19,42 @@ C_Di = induced coeff of drag
 %}
 
 % solve linearly
-y = linspace(0,b/2,100);
-for i= 1:size(y)
-if y(i) > -b/2 && y(i) < 0
-    c = c_r + (c_r-c_t)*y/(b/2);
-    a0 = a0_r + (a0_r-a0_t)*y/(b/2);
-    aero = aero_r + (aero_r-aero_t)*y/(b/2);
-    geo = geo_r + (geo_r-geo_t)*y/(b/2);
-else 
-    c = c_r + (c_t-c_r)*y/(b/2);
-    a0 = a0_r + (a0_t-a0_r)*y/(b/2);
-    aero = aero_r + (aero_t-aero_r)*y/(b/2);
-    geo = geo_r + (geo_t-geo_r)*y/(b/2);
-end
-end
-
-% left hand side is effective angle of attack
-alpha_eff = geo - aero;
-n_odd = 1:2:N;
-
-M = zeros(N,N);
-theta = zeros(1,N);
 for i=1:N
     theta(i) = i*pi/(2*N);
 end
-for i=1:size(a0)
-    for j=1:size(a0)
-    n = n_odd(j);
-    test(i,j) = sin(n*theta(i));
-    test2(i,j) = 4*b/(a0(i)*c(i));
-    test3(i,j) = n/sin(theta(i));
-    M(i,j) = sin(n*theta(i)) * (4*b./(a0(i)*c(i)) + n/sin(theta(i)));
+
+c = c_r + (c_t-c_r)*cos(theta);
+a0 = a0_r + (a0_t-a0_r)*cos(theta);
+aero = aero_r + (aero_t-aero_r)*cos(theta);
+geo = geo_r + (geo_t-geo_r)*cos(theta);
+
+% left hand side is effective angle of attack
+alpha_eff = geo - aero;
+
+M = zeros(N,N);
+theta = zeros(1,N);
+
+for i=1:length(N)
+    for j=1:length(N)
+    n = 2*j - 1;
+    M(i,j) = (4*b/(a0(i)*c(i)) * sin(n*theta(i))) + n*sin(n*theta(i))/sin(theta(i));
     end
 end
 
-A = M ./ alpha_eff;
+A = M \ alpha_eff';
 
 delta = 0;
+e = 0;
+c_L = 0;
+c_Di = 0;
 
-% for j = 2:N
-%     n = n_odd(j);
-%     delta = delta + n*(A(i)/A(1))^2;
-% end
-% 
-% e = 1 / (1 + delta);
-% 
-% S = b*((c_r - c_t)/2 + c_t);
-% c_L = A(1) * pi * b^2 / S;
+for j = 2:N
+    n = 2*j -1;
+    delta = delta + n*(A(i)/A(1))^2;
+end
 
+S = b*((c_r - c_t)/2 + c_t);
+AR = b^2 / S;
+e = 1 / (1 + delta);
+c_L = A(1)*pi*AR;
+c_Di = c_L^2 / (pi*e*AR);
